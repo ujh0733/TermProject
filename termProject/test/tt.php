2b6a187993d4dd14b207e828d4f8ec4f5d95c847
ujh0733 <ujh0733@naver.com>
2018-10-26 16:50:10 +0900

    code detach

--- termProject/test/tt.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<a href="location.href='www.naver.com'" class="btn btn-danger" onclick="return confirm('삭제할래?')">Delete</a>
-</body>
-</html>
